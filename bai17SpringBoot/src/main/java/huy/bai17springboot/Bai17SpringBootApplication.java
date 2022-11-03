ff5f5c83ca632a359c46d770cff7c51f01bfb5f9
DucMinhHuy <ducminhhuy99tbvn2@gmail.com>
2022-11-03 20:24:12 +0700

    tesst

---
+++ bai17SpringBoot/src/main/java/huy/bai17springboot/Bai17SpringBootApplication.java
@@ -0,0 +1,13 @@
+package huy.bai17springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bai17SpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bai17SpringBootApplication.class, args);
+    }
+
+}
---
+++ bai17SpringBoot/src/test/java/huy/bai17springboot/Bai17SpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package huy.bai17springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bai17SpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
