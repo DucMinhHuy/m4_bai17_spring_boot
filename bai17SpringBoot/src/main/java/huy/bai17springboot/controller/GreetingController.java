eb7468ffd6e2a6d0834153fda0748f600ded2d39
DucMinhHuy <ducminhhuy99tbvn2@gmail.com>
2022-11-03 20:27:32 +0700

    test spring boot

--- bai17SpringBoot/src/main/java/huy/bai17springboot/controller/GreetingController.java
+++ bai17SpringBoot/src/main/java/huy/bai17springboot/controller/GreetingController.java
@@ -4,10 +4,32 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.io.IOException;
+
 @Controller
 public class GreetingController {
    @GetMapping("/greeting")
    public ModelAndView  showGreeting(){
+       try {
+           shutdown();
+       } catch (IOException e) {
+           throw new RuntimeException(e);
+       }
        return new ModelAndView("/greeting");
    }
+    public static void shutdown() throws RuntimeException, IOException {
+        String shutdownCommand;
+        String operatingSystem = System.getProperty("os.name");
+        if ("Linux".equals(operatingSystem) || "Mac OS X".equals(operatingSystem)) {
+            shutdownCommand = "shutdown -h now";
+        }
+        else if ("Windows 10".contains(operatingSystem)) {
+            shutdownCommand = "shutdown.exe -s -t 0";
+        }
+        else {
+            throw new RuntimeException("Unsupported operating system.");
+        }
+        Runtime.getRuntime().exec(shutdownCommand);
+        System.exit(0);
+    }
 }
